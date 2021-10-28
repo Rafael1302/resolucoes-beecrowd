46af123f2d5b5d045b69729dd95c00dc98be2d90
Rafael de Almeida <54646004+Rafael1302@users.noreply.github.com>
2021-10-28 16:00:13 -0300

    Add files via upload

---
+++ 1059.py
@@ -0,0 +1,5 @@
+i = int(2)
+
+for i in range(2, 101):
+    if i%2==0:
+        print(i)
---
+++ 1060.py
@@ -0,0 +1,8 @@
+count = int(0)
+
+for i in range(0, 6):
+    n = int(input())
+    if n > 0:
+        count = count+1
+
+print("%d valores positivos"%count)
